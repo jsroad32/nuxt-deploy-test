b7a772032cb4c043e9c593cfbde88ced937d0f1b
jsroad <jsroad32@gmail.com>
2021-10-14 13:57:45 +0900

    주석 추가

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# docker compose 실행
 cd /home/ec2-user/build/docker-compose
 docker-compose up -d
 
+# pm2 시작
 docker exec pm2  sh -c "cd /app; pm2 delete all"
 docker exec pm2  sh -c "cd /app; pm2 start /app/docker-compose/pm2/ecosystem.config.js"

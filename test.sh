296ad3701a1a840bcd6db04af1eb7d0d7afe5a77
jsroad <jsroad32@gmail.com>
2021-10-14 13:50:40 +0900

    docker shell 조정

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-export WORK_HOME=/home/ec2-user/build
-
-cd WORK_HOME/docker-compose
+cd /home/ec2-user/build/docker-compose
 docker-compose up -d
 
+docker exec pm2  sh -c "cd /app; pm2 delete all"
 docker exec pm2  sh -c "cd /app; pm2 start /app/docker-compose/pm2/ecosystem.config.js"

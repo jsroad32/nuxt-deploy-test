8cfe8007bbdd2096b373e05b24b82c5b6ea82f19
jsroad <jsroad32@gmail.com>
2021-11-12 23:17:48 +0900

    shell 실행 일시중지

--- docker-compose/pm2/ecosystem.config.js
+++ docker-compose/pm2/ecosystem.config.js
@@ -6,11 +6,6 @@ module.exports = {
       instances: 'max', // Or a number of instances
       script: '/app/node_modules/nuxt/bin/nuxt.js',
       args: 'start',
-
-      // wait_ready: true, // ready 신호 대기
-      // listen_timeout: 50000, // ready 이벤트 대기 시간(ms)
-      // kill_timeout: 5000 //
-
       // script: 'npm',
       // args: 'run start',
       // env: {

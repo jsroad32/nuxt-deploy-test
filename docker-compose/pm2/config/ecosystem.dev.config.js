b9d795777b40a1668c13f65a729c519778fa2fee
jsroad <jsroad32@gmail.com>
2021-11-15 14:23:36 +0900

    환경 설정 파일 조정

--- docker-compose/pm2/config/ecosystem.dev.config.js
+++ docker-compose/pm2/config/ecosystem.dev.config.js
@@ -3,11 +3,12 @@ module.exports = {
     {
       name: 'dev',
       exec_mode: 'cluster',
-      instances: '1', // Or a number of instances
+      instances: '1', // 클러스터 모드 사용 시 생성할 인스턴스 수
       // script: '/app/node_modules/nuxt/bin/nuxt.js',
       // script: './node_modules/nuxt/bin/nuxt.js',
       script: 'npm',
       args: 'start',
+      watch: true, // 파일이 변경되었을 때 재시작 할지 선택
     }
   ]
 }

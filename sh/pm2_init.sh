f7b42e7d03f32be16f503a88aa3f9b744bc453d8
jsroad <jsroad32@gmail.com>
2021-11-15 17:17:38 +0900

    pm2 init 디렉토리 조정

--- sh/pm2_init.sh
+++ sh/pm2_init.sh
@@ -2,10 +2,9 @@
 
 # docker compose 실행
 sudo systemctl start docker
-cd /home/ec2-user/build/docker-compose
+cd /home/app/build/docker-compose
 docker-compose up -d
 
 # pm2 시작
 #docker exec pm2  sh -c "cd /app; pm2 delete all"
 docker exec pm2  sh -c "cd /app; pm2 start ./docker-compose/pm2/config/ecosystem.dev.config.js"
-

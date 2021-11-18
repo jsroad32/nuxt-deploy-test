6f496dfd3c08d699d31869a0dcbd082be80e1369
jsroad <jsroad32@gmail.com>
2021-11-18 16:16:25 +0900

    test

--- sh/pm2_init.sh
+++ sh/pm2_init.sh
@@ -5,6 +5,10 @@ sudo systemctl start docker
 cd /home/app/build/docker-compose
 docker-compose up -d
 
+# build
+docker exec pm2  sh -c "cd /app; npm install"
+docker exec pm2  sh -c "cd /app; npm run build"
+
 # pm2 시작
 #docker exec pm2  sh -c "cd /app; pm2 delete all"
 docker exec pm2  sh -c "cd /app; pm2 start ./docker-compose/pm2/config/ecosystem.dev.config.js"
